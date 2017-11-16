3da00f565ffcd856066fab4934e44bc8f7e591ba
Sam <Hussamnachabe@gmail.com>
2017-11-16 14:07:40 +0100

    Accueil Fonctionne correctement :)

--- HopitalBim/Accueil.php
+++ HopitalBim/Accueil.php
@@ -24,10 +24,10 @@ if(isset($_POST['btn-login']))
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage : Login</title>
+<title>Accueil</title>
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
 <link rel="stylesheet" href="style.css" type="text/css"  />
@@ -41,7 +41,7 @@ if(isset($_POST['btn-login']))
         
        <form class="form-signin" method="post" id="login-form">
       
-        <h2 class="form-signin-heading">Log In to WebApp.</h2><hr />
+        <h2 class="form-signin-heading">Authentification</h2><hr />
         
         <div id="error">
         <?php
@@ -49,7 +49,7 @@ if(isset($_POST['btn-login']))
 			{
 				?>
                 <div class="alert alert-danger">
-                   <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                   <i class=""></i><?php echo $error; ?> !
                 </div>
                 <?php
 			}
@@ -57,23 +57,22 @@ if(isset($_POST['btn-login']))
         </div>
         
         <div class="form-group">
-        <input type="text" class="form-control" name="txt_uname" placeholder="Username or E mail ID" required />
+        <input type="text" class="form-control" name="txt_uname" placeholder="Nom d'utilisateur !" required />
         <span id="check-e"></span>
         </div>
         
         <div class="form-group">
-        <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
+        <input type="password" class="form-control" name="txt_password" placeholder="Votre mot de passe!" />
         </div>
        
      	<hr />
         
         <div class="form-group">
             <button type="submit" name="btn-login" class="btn btn-default">
-                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN
+                	<i class=""></i>Valider
             </button>
         </div>  
       	<br />
-            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
       </form>
 
     </div>
