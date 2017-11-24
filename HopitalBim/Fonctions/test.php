233d7e578d9925ca2207895b3cbb37b77c429564
Sam <Hussamnachabe@gmail.com>
2017-11-24 15:48:27 +0100

    la fiche patient fonctionne enfin putin apres 10k de tests

---
+++ HopitalBim/Fonctions/test.php
@@ -0,0 +1,15 @@
+<?php
+function 
+
+
+function TestBlockHTML () {
+	 echo 'GG'; 
+	 print ("<input type='text' class='form-control' name='text_pays'   pattern='[A-Za-z]{1-25}' title='Caractère alphabetique, 25 caractères maximum' placeholder='Entrer le pays :' value='");
+	if(isset($error)){echo $text_pays;}
+	print ("' /><br>");
+
+}
+?>
+
+
+
