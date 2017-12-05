8e75b68b2052880d97de2d0193aada952ba57d66
Unknown <poullet.m@hotmail.fr>
2017-12-05 11:33:44 +0100

    Page&Formulaire Patient Créer et Modifier

--- HopitalBim/Formulaires/RecherchePatient.php
+++ HopitalBim/Formulaires/RecherchePatient.php
@@ -3,16 +3,16 @@
 if(isset($_POST['btn_facturation'])) // action du bouton btn_facture
 {	 
 	$text_numSS = preg_replace("/[^0-9]/", "",trim($_POST['text_patient'], ' '));
-	// Gestion erreur : idPatient existe pas dans la bdd
+	// Gestion erreur : idPatient n'existe pas dans la bdd
 		$req_numSS = $auth_user->runQuery(" SELECT numSS
 											FROM Patients
 											WHERE numSS = :numSS" ); // recherche le numSS dans la bdd
 		$req_numSS->execute(array('numSS'=> $text_numSS));
 		$numSS= $req_numSS-> fetchColumn();
 
-		if ($numSS == "") // nom de d'INTERVENTION absent de la base de donnée
+		if ($numSS == "") // nom de d'INTERVENTION absent de la base de données
 		{
-			$error[] =  "Saisir un numéro de sécurité sociale valide";
+			$error[] =  "Veuillez saisir un numéro de sécurité sociale valide !";
 		}
 		else 
 		{
@@ -26,80 +26,82 @@ if(isset($_POST['btn_facturation'])) // action du bouton btn_facture
 
 <!DOCTYPE html PUBLIC >
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<title>Demande RDV</title>
-</head>
+	<head>
+		<title>Demande RDV</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<link rel="stylesheet" href="../Config/Style.css" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
+	</head>
 
-<body>
+	<body>
+		<div class="containerFormu">
 
-			<p class="" style="margin-top:5px;">
-				<div class="signin-form">
-					<form method="post" class="form-signin">
+			<form method="post" class="form-signin">
 								
-								<h2 class="form-signin-heading">Rechercher un patient </h2><hr />
-	<?php
-								if(isset($error)) // affichage messages erreurs si valeurs != format attendu pour les champs
-								{
-									foreach($error as $error) // pour chaque champs
-									{
-	?>
-										<div class="alert alert-danger">
-										<i class=""></i> &nbsp; <?php echo $error; ?>
-										</div>.
-	<?php
-									}
-								}
-								else if(isset($_GET['Valide'])) // si toutes les valeurs de champs ok et que bouton valider
-								{
-	?>
-									<div class="alert alert-info">
-									<i class=""></i> Rendez-vous fixé le (date) à (heure) <a href='../Pageprincipale.php'>Page principale</a>
-									<!--insertion fonction pour editer pdf--> 
-									</div>
-	<?php
-								}
-	?>
-								<!-- Affichage formulaire -->
-								<fieldset>
-									<legend> Nom et prénom patient </legend> <!-- Titre du fieldset --> 
-										<p>
-											<!-- Affichage formulaire : moteur recherche du patient-->
-											<input list="text_patient" name="text_patient" size='85'> 
-											<datalist id="text_patient" >
-	<?php 
-												$req_patient = $auth_user->runQuery("SELECT numSS, nom, prenom FROM Patients"); // permet de rechercher le nom d utilisateur 
-												$req_patient->execute(); // la meme 
-												while ($row_patient = $req_patient->fetch(PDO::FETCH_ASSOC))
-												{
-													echo "<option value='".$row_patient['numSS']."' 
-													label='"."(".$row_patient['numSS'].")"." -- ".$row_patient['prenom']." ".$row_patient['nom']."'>".$row_patient['prenom']." ".$row_patient['nom']."</option>";
-												}
-	?>
-											</datalist>
-										</br >
-										</p>
-								</fieldset>					
-							
-								<!-- bouton validé -->
-						</div>
-					<div class="clearfix"></div>
-					<hr />
-					<div class="form-group">
-						<button type="submit" class="btn btn-primary" name="btn_facturation">
-							<i class=""></i>Valider
-						</button>
-	<?php
-						quitter1($auth_user)
-	?>	
+				<h2 class="form-signin-heading">Rechercher un patient </h2><hr />
+
+					<?php
+						if(isset($error)) // affichage messages erreurs si valeurs != format attendu pour les champs
+						{
+							foreach($error as $error) // pour chaque champs
+							{
+					?>
+						<div id="error"> &nbsp; <?php echo $error; ?> </div>
+					
+					<?php
+						}
+					}
+						else if(isset($_GET['Valide'])) // si toutes les valeurs de champs ok et que bouton valider
+						{
+					?>
+
+					<div id="valide">
+						Rendez-vous fixé le (date) à (heure) <a href='../Pageprincipale.php'>Page principale</a>
+						<!--insertion fonction pour editer pdf--> 
 					</div>
-				</form>
-			</p>
-			
 	
+					<?php
+						}
+					?>
+
+				<fieldset>
+				<legend> Nom et prénom patient </legend> <!-- Titre du fieldset --> 
+										
+					<!-- Affichage formulaire : moteur recherche du patient-->
+					<label for="text_patient"> Patient </label>
+					<input list="text_patient" name="text_patient" size='85'> 
+					<datalist id="text_patient" >
 	
-		
-</body>
+						<?php 
+							$req_patient = $auth_user->runQuery("SELECT numSS, nom, prenom FROM Patients"); // permet de rechercher le nom d utilisateur 
+							$req_patient->execute(); // la meme 
+							while ($row_patient = $req_patient->fetch(PDO::FETCH_ASSOC))
+							{
+								echo "<option value='".$row_patient['numSS']."' 
+								label='"."(".$row_patient['numSS'].")"." -- ".$row_patient['prenom']." ".$row_patient['nom']."'>".$row_patient['prenom']." ".$row_patient['nom']."</option>";
+							}
+						?>
+											
+					</datalist>
+				
+				</fieldset>	</br >				
+					
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary" name="btn_facturation">
+					Valider
+					</button>
+				</div>
 
+					
+			</form>
+		</div> <!-- containerFormu -->
 
+		<div class="abandon">
+			<?php
+				quitter1($auth_user)
+			?>	
+		</div> <!-- abandon -->
+
+	</body>
 </html>
