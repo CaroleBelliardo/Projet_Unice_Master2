b01d70128d378db88a70103fb70cba8943a06504
Carole Belliardo <belliardo.carole@hotmail.fr>
2017-12-07 17:27:40 +0100

    acte ajout

--- HopitalBim/Formulaires/Formulaire_AjoutActe.php
+++ HopitalBim/Formulaires/Formulaire_AjoutActe.php
@@ -1,90 +1,85 @@
+<?php // action quand valide le formulaire 
+	if(isset($_POST['btn-ajoutActe']))
+	{
+		$text_nomActe = ucfirst(trim($_POST['text_nomActe']));	
+		$text_tarif = trim($_POST['text_tarif']);	
+
+	// Gestion des erreurs : 
+		if ($text_nomActe==""){$error[] = "Il faut un entrer le nom de l'acte !"; }
+		elseif ($text_tarif==""){$error[] = "Il faut un entrer le tarif !"; }	
+		else 
+		{ 
+			$req_existeActe = $auth_user->runQuery("SELECT idIntervention 
+							FROM Interventions
+							WHERE acte = :acte
+							AND ServicesnomService = :nomService");
+			$req_existeActe->execute(array("nomService"=>$_SESSION['serviceModifier'],
+										"acte"=>$text_nomActe));
+			$existeActe=$req_existeActe-> fetch(PDO::FETCH_ASSOC);
+			if ($existeActe != FALSE){$error[] = "Cet acte existe déjà pour ce service !"; }
+			else
+			{
+				$req_insertActe = $auth_user->runQuery("INSERT INTO Interventions (acte, ServicesnomService) 
+									VALUE ( :acte, :nomService)");
+				$req_insertActe->execute(array('acte'=>$text_nomActe,
+								'nomService'=>$_SESSION['serviceModifier']));
+				$req_insertActe->closeCursor();
+				
+				$req_idLastActe = $auth_user->runQuery("SELECT MAX(idIntervention) FROM Interventions ");
+				$req_idLastActe->execute();
+				$idLastActe = $req_idLastActe -> fetchColumn();
+				$req_idLastActe->closeCursor();
+				
+				$req_insertActeTarif = $auth_user->runQuery("INSERT INTO Tarifications (InterventionsidIntervention, tarif_euros) 
+									VALUE ( :id, :tarif)");
+				$req_insertActeTarif->execute(array('id'=>$idLastActe,
+								'tarif'=> floatval($text_tarif)));
+				$req_insertActeTarif->closeCursor();
+				$auth_user->redirect('ActeCreer.php?Valide');
+			}
+		}
+	}
+?>
+<!--mise en page du formulaire-->
 <div class="containerFormu">
-    	
         <form method="post" class="form-signin">
-
             <h2 class="form-signin-heading">Ajouter un nouveau service</h2> <hr />
-
-            <?php
-			if(isset($error))
+        <?php
+		if(isset($error))
+		{
+			foreach($error as $error)
 			{
-			 	foreach($error as $error)
-			 	{
-					 ?>
-                     <div id="error"> &nbsp; <?php echo $error; ?> </div> <!-- Alert alert-danger-->
+	?>
+                <div id="error"> &nbsp; <?php echo $error; ?> </div> <!-- Alert alert-danger-->
 
-                     <?php
-				}
+        <?php
 			}
-			else if(isset($_GET['Valide']))
-			{
-			?>
-                 <div id="valide"> <!-- Alert alert-info-->
-                      Service ajouté avec succés ! <a href='../Pageprincipale.php'> Page principale </a>
-                 </div>
-            <?php
-			}
-			?>
-			
-			<p><i>Complétez le formulaire. Les champs marqués par </i><em>*</em> sont <em>obligatoires.</em></p>
-
-			<div class="form-group">
+		}
+		else if(isset($_GET['Valide']))
+		{
+	?>
+		<div id="valide"> <!-- Alert alert-info-->
+		     L'acte médical ajouté avec succés ! <a href='../Pageprincipale.php'> Page principale </a>
+		</div>
+        <?php
+		}
+	?>
+		<p><i>Complétez le formulaire. Les champs marqués par </i><em>*</em> sont <em>obligatoires.</em></p>
+		<div class="form-group">
 			<fieldset>
-			<legend> Service </legend> <!-- Titre du fieldset --> 
+			<legend> L'acte médical </legend> <!-- Titre du fieldset --> 
 
-				<label for="text_nomService">Nom du service <em>* </em> </label>
-				<input type="text" class="form-control" name="text_nomService" pattern="[A-Za-z]{1-20}" title="Majuscule en première lettre"        placeholder=" Service" value="<?php if(isset($error)){echo $text_nomService;}?>" /><br>
+				<label for="text_nomActe">Nom de l'acte <em>* </em> </label>
+				<input type="text" class="form-control" name="text_nomActe" pattern="[A-Za-z]{1-35}" title="Majuscule en première lettre"        placeholder=" Service" value="<?php if(isset($error)){echo $text_nomActe;}?>" /><br>
 
-				<label for="text_telephone">Téléphone </label>
-				<input type="tel" class="form-control" name="text_telephone" pattern="[0-9]{1-15}" title="Veuillez rentrer un n° de téléphone correct"    placeholder=" 06xxxxxxxx " value="<?php if(isset($error)){echo $text_telephone;}?>" /><br>
+				<label for="text_tarif">Tarif (en €)  <em>* </em></label>
+				<input type="tel" class="form-control" name="text_tarif" pattern="[0-9]{1-10}" title="Veuillez rentrer un n° de téléphone correct"    placeholder=" " value="<?php if(isset($error)){echo $text_tarif;}?>" /><br>
+			</fieldset>
+		</div> <!-- form-group  Formulaire principal --> 
 
-				<label for="text_ouverture">Horaire d'ouverture </label>
-				<input type="time" class="form-control" name="text_ouverture" value="<?php if(isset($error)){echo $text_ouverture;}?>" /><br>
-
-				<label for="text_fermeture">Horaire de fermeture </label>
-				<input type="time" class="form-control" name="text_fermeture" value="<?php if(isset($error)){echo $text_fermeture;}?>" /><br>
-
-			</fieldset> <br>
-			
-			<fieldset>
-			<legend> Localisation </legend> <!-- Titre du fieldset --> 
-
-				<label for="text_batiment">Bâtiment </label>  
-				<select name="text_batiment">
-					<option value="A">A</option>
-					<option value="B">B</option>
-					<option value="C">C</option>
-					<option value="D">D</option>
-					<option value="E">E</option>
-					<option value="F">F</option>
-				</select><br> 
-
-				<label for="text_etage">Étage </label>  
-				<select name="text_etage"> 
-					<option value="0">RDC</option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="-1">-1</option>
-				</select><br>
-
-				<label for="text_aile">Aile </label>  
-				<select name="text_aile">
-					<option value="a">a</option>
-					<option value="b">b</option>
-					<option value="c">c</option>
-					<option value="d">d</option>
-					<option value="e">e</option>
-					<option value="f">f</option>
-				</select>
-			
-			</fieldset> <br>
-			</div> <!-- form-group // Formulaire principal --> 
-
-            <div class="form-group">
-            	<button type="submit" class="btn btn-primary" name="btn-valider">Valider</button>
-            </div> <!-- form-group // Bouton Valider -->
-
+		<div class="form-group">
+		    <button type="submit" class="btn btn-primary" name="btn-ajoutActe">Valider</button>
+		</div> <!-- form-group  Bouton Valider -->
         </form> <!-- form-signin -->
 
 </div> <!-- containerFormu -->
