a3a4212006ef758bc166a12d1207098388afd481
Carole Belliardo <belliardo.carole@hotmail.fr>
2017-12-09 03:30:30 +0100

    rapport

--- HopitalBim/Fonctions/Fonctions_Affichage.php
+++ HopitalBim/Fonctions/Fonctions_Affichage.php
@@ -7,48 +7,6 @@ function Dumper ($var){ // affichage des valeurs des tableaux
 }
 
 
-function liste_Services($auth_user) // affiche un menu déroulant listant les services proposé par l'hopital
-	{ 
-?>
-	<div class="form-group" >
-		<select name="text_nomService">
-<?php 
-			$stmt = $auth_user->runQuery("SELECT nomService FROM Services"); // permet de rechercher le nom d utilisateur 
-			$stmt->execute(); // la meme 
-			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-			echo "<option value='".$row['nomService']."'>".$row['nomService']."</option>";
-			} ?>
-			</select>
-		</div>
-	
-<?php
-	}
-
-
-
-function Affiche_dateFr($d) // AFFICHE la date au format francais (ex : jeudi 2 novembre 2017)
-	{
-		setlocale(LC_TIME, 'fr_FR.utf8','fra'); 
-		$date_time = new DateTime($d);
-		
-		$intl_date_formatter = new IntlDateFormatter('fr_FR',
-													 IntlDateFormatter::FULL,
-													 IntlDateFormatter::NONE);
-		echo $intl_date_formatter->format($date_time);
-	}
-
-
-
-	
-function test($auth_user)
-	{ 
-?>
-	<div class="form-group" >
-		<input type="text" class="" name="text_numSS" pattern="[0-9]{15}" title="Caractère numérique, 15 caractères acceptés"        placeholder="Numero Securité Sociale :" value="<?php if(isset($error)){echo $text_numSS;}?>" /><br>
-	</div>
-	
-<?php
-	}
 
 
 function quitter1()
